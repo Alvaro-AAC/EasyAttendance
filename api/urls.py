6875e0b82446f65cbd5a11331ce58906b644ef29
Alvaro Arenas <aarenas@analytics.cl>
2023-05-18 22:25:07 -0400

    ADD: Url serial

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('v1/testing/', testing),
+    path('v1/pasar_asistencia/', pasar_asistencia),
     path('v1/lista_asistencia/<id>/', lista_asistencia),
     path('v1/registrar_asistencia/<token_clase>/<token_usuario>/', registrar_asistencia),
     path('v1/generar_codigo_clase/', codigo_asistencia),
